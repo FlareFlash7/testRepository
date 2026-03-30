b547dacbd5b0f3c24e7a3b1da3b6a90613075f90
FlareFlash7 <jputnam@oakland.edu>
2026-03-30 09:41:49 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
- 
+ /*make the animal counter*/
+let animalsOnScreen = 0;
+/*update the counter as it continues*/
+function updateCounter() {
+  document.getElementById("counter").textContent =
+    "Animals on Screen: " + animalsOnScreen;
+}
+/*keeps track of which animal*/
+function toggleAnimal(id) {
+  const img = document.getElementById(id);
+
+  if (img.style.display === "none") {
+    img.style.display = "block";
+    animalsOnScreen++;
+  } else {
+    img.style.display = "none";
+    animalsOnScreen--;
+  }
+
+  updateCounter();
+}
+/*can reset everything*/
+function resetZoo() {
+  const animals = ["cat", "dog", "cow"];
+
+  animals.forEach(id => {
+    const img = document.getElementById(id);
+    img.style.display = "none";
+  });
+
+  animalsOnScreen = 0;
+  updateCounter();
+}
